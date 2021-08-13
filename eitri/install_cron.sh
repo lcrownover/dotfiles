74cf7b7b20eb6df184c1ba9d4494715c991180ee
Lucas Crownover <lcrown@uoregon.edu>
2021-08-13 14:07:39 -0700

    working on cron

--- eitri/install_cron.sh
+++ eitri/install_cron.sh
@@ -9,5 +9,9 @@ fi
 
 test -f report.py || (echo "You must run this script from where report.py is saved"; exit 1)
 
-sed -i "s|PATH|$(pwd)/report.py|g" ip_report.cron
-cp -f ip_report.cron /etc/cron.d/ip_report.cron
+CRONFILE="ip_report"
+
+cp -f $CRONFILE /etc/cron.d/$CRONFILE
+sed -i "s|PATH|$(pwd)/report.py|g" /etc/cron.d/$CRONFILE
+chmod 644 /etc/cron.d/$CRONFILE
+chown root:root /etc/cron.d/$CRONFILE
