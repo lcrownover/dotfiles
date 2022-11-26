44c46c188c5020a2112ad718138a60a97ff5c474
Lucas Crownover <lcrownover127@gmail.com>
2022-11-25 17:36:35 -0800

    podman for mac only

--- zsh/zsh/50-podman.sh
+++ zsh/zsh/50-podman.sh
@@ -1,4 +1,5 @@
 if [ "$OS" = "mac" ]; then
     export DOCKER_HOST="unix://$HOME/.local/share/containers/podman/machine/podman-machine-default/podman.sock"
+    alias docker='podman'
+    source <(podman completion zsh)
 fi
-alias docker='podman'
