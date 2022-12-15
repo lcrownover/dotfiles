0ed7b9f98bdfdd8de5ee0173494225a778adbe54
Lucas Crownover <lcrownover127@gmail.com>
2022-12-14 22:03:39 -0800

    fixed null ls

--- nvim/lua/lcrownover/plugins/lsp/mason.lua
+++ nvim/lua/lcrownover/plugins/lsp/mason.lua
@@ -1,6 +1,7 @@
 local mason = require('mason')
 local mason_lspconfig = require('mason-lspconfig')
 local mason_null_ls = require('mason-null-ls')
+local null_ls = require('null-ls')
 
 mason.setup()
 
@@ -31,11 +32,22 @@ mason_lspconfig.setup({
 mason_null_ls.setup({
   automatic_installation = true,
   ensure_installed = {
-    'blue',
+    'black',
     'rubocop',
     'stylua',
     'jq',
     'shfmt',
     'markdownlint',
   },
+})
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.jq,
+        null_ls.builtins.formatting.markdownlint,
+        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.formatting.shfmt,
+    },
 })
