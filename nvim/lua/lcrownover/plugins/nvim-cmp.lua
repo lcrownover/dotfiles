8274d46ce45e9f7af379d9d069392b9f269ebb8b
Lucas Crownover <lcrownover127@gmail.com>
2022-12-28 12:28:36 -0800

    fix for lsp

--- nvim/lua/lcrownover/plugins/nvim-cmp.lua
+++ nvim/lua/lcrownover/plugins/nvim-cmp.lua
@@ -1,34 +1,24 @@
 local cmp = require("cmp")
-local luasnip = require("luasnip")
-local lspkind = require('lspkind')
-
--- load vs-code like snippets from plugins (e.g. friendly-snippets)
-require("luasnip/loaders/from_vscode").lazy_load()
+local lspkind = require("lspkind")
 
 vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
 
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-
+cmp.setup({
   mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
     ["<C-j>"] = cmp.mapping.select_next_item(),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm {
+    ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
-      select = true
-    }
+      select = true,
+    }),
   },
 
   -- sources for autocompletion
   sources = {
     { name = "nvim_lsp" }, -- lsp
-    { name = "luasnip" }, -- snippets
+    -- { name = "luasnip" }, -- snippets
     { name = "buffer" }, -- current buffer
     { name = "path" }, -- os path
   },
@@ -36,11 +26,11 @@ cmp.setup {
   -- configure completion icons
   formatting = {
     format = lspkind.cmp_format({
-      mode = 'symbol',
+      mode = "symbol",
       maxwidth = 50,
       ellipsis_char = "...",
-    })
+    }),
   },
 
   min_length = 0, -- allow for `from package import _` in Python
-}
+})
