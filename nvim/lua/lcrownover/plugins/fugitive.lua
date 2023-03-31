f14c172c318dc3789f799f09f91f9b6596a2eed6
Lucas Crownover <lcrownover127@gmail.com>
2023-03-31 09:52:45 -0700

    Added binds for push and pull

--- nvim/lua/lcrownover/plugins/fugitive.lua
+++ nvim/lua/lcrownover/plugins/fugitive.lua
@@ -4,8 +4,8 @@ return {
   config = true,
   keys = {
     { "<leader>gs", ":Git<cr>" },
-    { "<leader>gc", ":Git commit<cr>" },
-    { "<leader>gp", ":Git push<cr>" },
+    { "<leader>gp", ":Git pull<cr>" },
+    { "<leader>gP", ":Git push<cr>" },
     { "<leader>gb", ":Git blame<cr>" },
     { "<leader>gd", ":Gvdiffsplit<cr>" },
     { "<leader>gh", ":diffget //2<cr>" },
