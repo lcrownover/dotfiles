ed285f0739e52d05d1c0a94eefd3837050c77012
Lucas Crownover <lcrownover127@gmail.com>
2023-05-07 16:42:01 -0700

    tried tab ugh

--- nvim/lua/lcrownover/plugins/cmp.lua
+++ nvim/lua/lcrownover/plugins/cmp.lua
@@ -18,6 +18,13 @@ return {
 
     vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
 
+    -- This is for copilot
+    -- local has_words_before = function()
+    --   if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
+    --   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    --   return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+    -- end
+
     cmp.setup({
       mapping = {
         ["<C-k>"] = cmp.mapping.select_prev_item(),
@@ -28,6 +35,13 @@ return {
           behavior = cmp.ConfirmBehavior.Replace,
           select = false,
         }),
+        -- ["<Tab>"] = vim.schedule_wrap(function(fallback)
+        --   if cmp.visible() and has_words_before() then
+        --     cmp.select_next_item()
+        --   else
+        --     fallback()
+        --   end
+        -- end),
       },
       window = {
         completion = cmp.config.window.bordered(),
@@ -40,11 +54,11 @@ return {
       },
       -- sources for autocompletion
       sources = {
-        { name = "copilot", priority = 5 }, -- copilot
+        { name = "copilot",  priority = 5 }, -- copilot
         { name = "nvim_lsp", priority = 4 }, -- lsp
-        { name = "luasnip", priority = 3 }, -- snippets
-        { name = "path", priority = 2 }, -- os path
-        { name = "buffer", priority = 1 }, -- current buffer
+        { name = "luasnip",  priority = 3 }, -- snippets
+        { name = "path",     priority = 2 }, -- os path
+        { name = "buffer",   priority = 1 }, -- current buffer
       },
       -- configure completion icons
       formatting = {
