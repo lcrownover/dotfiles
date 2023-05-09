d450de78ccd10e3452e78e327718c0131a92eef2
Lucas Crownover <lcrownover127@gmail.com>
2023-05-09 15:22:28 -0700

    theme

--- nvim/lua/lcrownover/plugins/theme.lua
+++ nvim/lua/lcrownover/plugins/theme.lua
@@ -9,7 +9,7 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-        vim.g.everforest_background = "soft"
+        vim.g.everforest_background = "medium"
         vim.g.everforest_better_performance = 1
         vim.cmd([[colorscheme everforest]])
         vim.g.lualine_theme = "everforest"
