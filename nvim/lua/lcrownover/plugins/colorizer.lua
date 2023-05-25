f41e9bdb26e24a23e7a7b04ca107cfcda76a5113
Lucas Crownover <lcrownover127@gmail.com>
2023-05-25 15:15:12 -0700

    using mini for a few things

--- nvim/lua/lcrownover/plugins/colorizer.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "norcalli/nvim-colorizer.lua",
-  config = function()
-    require("colorizer").setup({ "*" }, { names = false })
-  end,
-}
--- zsh/zsh/50-hpc.sh
+++ zsh/zsh/50-hpc.sh
@@ -2,3 +2,6 @@ alias hpc='ssh talapas-ln1'
 alias gpra='spushd ~/ansible/racs/ansible; ssh ansible@talapas-hn1.uoregon.edu -i ssh/id_rsa "cd ~ansible/racs; git pull"; spopd'
 alias hpca='spushd ~/ansible/racs/ansible; ssh -t ansible@talapas-hn1.uoregon.edu -i ssh/id_rsa "cd ~ansible/racs/ansible; bash"; spopd'
 alias hpch='ssh lrc@talapas-hn1'
+
+alias t2='ssh login1.talapas.uoregon.edu'
+alias t2h='ssh head1.talapas.uoregon.edu'
