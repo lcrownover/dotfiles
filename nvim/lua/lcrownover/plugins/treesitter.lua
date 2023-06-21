269a3332ea9e967401a84ca67d53520a2d9a0e0a
Lucas Crownover <lcrownover127@gmail.com>
2023-06-21 13:45:15 -0700

    treesitter playground

--- nvim/lua/lcrownover/plugins/treesitter.lua
+++ nvim/lua/lcrownover/plugins/treesitter.lua
@@ -1,14 +1,31 @@
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    event = "BufEnter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = false, disable = { "html", "yaml", "python", "sh", "go" } },
-      })
-    end,
-  }
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = { "nvim-treesitter/playground" },
+	event = "BufEnter",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = false, disable = { "html", "yaml", "python", "sh", "go" } },
+			playground = {
+				enable = true,
+				disable = {},
+				updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+				persist_queries = false, -- Whether the query persists across vim sessions
+				keybindings = {
+					toggle_query_editor = "o",
+					toggle_hl_groups = "i",
+					toggle_injected_languages = "t",
+					toggle_anonymous_nodes = "a",
+					toggle_language_display = "I",
+					focus_language = "f",
+					unfocus_language = "F",
+					update = "R",
+					goto_node = "<cr>",
+					show_help = "?",
+				},
+			},
+		})
+	end,
 }
