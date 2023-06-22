ff05459f09d33c60d617e33c9fe64660cd309d54
Lucas Crownover <lcrownover127@gmail.com>
2023-06-22 10:15:21 -0700

    restore this

--- zsh/zsh/60-ssh.sh
+++ zsh/zsh/60-ssh.sh
@@ -1,3 +1,18 @@
+# catppuccin renames the left status to the current directory
+# so i just create a temp folder with the ssh hostname and cd to it before ssh
+function tssh() {
+	if [ $TMUX ]; then
+		mkdir -p /tmp/$*
+		cd /tmp/$*
+		spushd .
+	fi
+	TERM=screen-256color \ssh "$@"
+	if [ $TMUX ]; then
+		spopd
+	fi
+}
+alias ssh='tssh'
+
 # this function re-links all the ssh configs from keepass where the keys are stored
 function ensure_ssh_keys() {
 	mkdir -p $HOME/.ssh/keys
