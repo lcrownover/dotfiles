2024e77074c48fb4c911744d4d23fbceb4e2de2f
Lucas Crownover <lcrownover127@gmail.com>
2023-06-30 12:50:12 -0700

    dont need this

--- zsh/zsh/90-iterm-integration.sh
+++ zsh/zsh/90-iterm-integration.sh
@@ -1,2 +1,4 @@
-export ITERM2_SQUELCH_MARK=1
-test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh" || true
+if [ -n $TERM_PROGRAM ] && [ $TERM_PROGRAM = "iTerm.app" ]; then
+    export ITERM2_SQUELCH_MARK=1
+    test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh" || true
+fi
