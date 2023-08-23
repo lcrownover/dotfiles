3e30d6c57e6874c9f22e640dc9aeab762e651592
Lucas Crownover <lcrownover127@gmail.com>
2023-08-23 10:48:35 -0700

    maybe some changes for tmux?

--- zsh/zsh/02-tmux.sh
+++ zsh/zsh/02-tmux.sh
@@ -1,17 +1,37 @@
 alias tmux='tmux -2'
-alias t='tmux -2'
+alias t='start_tmux_session_with_pwd_name'
+
+# TMUX gets set in VSCode for some reason
+# so we can't use the default TMUX
+case "$TMUX" in
+"")
+    VTMUX=""
+    ;;
+*)
+    $(tmux ls 2>&1 | grep -q 'no server running')
+    if [ $? -eq 0 ]; then
+        VTMUX=""
+    else
+        VTMUX="$TMUX"
+    fi
+    ;;
+esac
+
+function start_tmux_session_with_pwd_name() {
+    [[ -n $VTMUX ]] || tmux new-session -s $(pwd)
+}
 
 function set_tmux_window_name() {
-    [[ -n "$TMUX" ]] && tmux rename-window $1
+    [[ -n $VTMUX ]] && tmux rename-window $1
 }
 
 function reset_tmux_window_name() {
     DEFAULT_TMUX_WINDOW_NAME="zsh"
-    [[ -n "$TMUX" ]] && tmux rename-window $DEFAULT_TMUX_WINDOW_NAME
+    [[ -n $VTMUX ]] && tmux rename-window $DEFAULT_TMUX_WINDOW_NAME
 }
 
 function main() {
-    [[ -n $TMUX ]] && OP="switch" || OP="attach"
+    [[ -n "$VTMUX" ]] && OP="switch" || OP="attach"
     $(tmux ls | grep -q main) || tmux new-session -d -s main
     tmux -2 $OP -t main
     # tmux -2 new-session -A -s main
@@ -19,7 +39,7 @@ function main() {
 
 function tmux_close_last_stay_open() {
     # dont do anything if not tmux
-    [[ -n $TMUX ]] || return
+    [[ -n $VTMUX ]] || return
 
     CURRENT_TMUX_WINDOW_ID=$(tmux list-windows | grep '(active)' | awk '{print $1}' | cut -d':' -f1)
     CURRENT_TMUX_PANE_ID=$(tmux list-panes | grep '(active)' | awk '{print $1}' | cut -d':' -f1)
@@ -66,58 +86,58 @@ project_switch() {
     fi
 
     case "$1" in
-        "a" | "add")
-            echo "$(pwd)" >>$PROJ_FILE
-            ;;
-        "e" | "edit")
-            $EDITOR $PROJ_FILE
-            ;;
-        "r" | "remove" | "rm")
-            if [ -z $2 ]; then
-                sed -i "" "/$(basename $(pwd))$/d" $PROJ_FILE
-            else
-                sed -i "" "/$2$/d" $PROJ_FILE
-            fi
-            ;;
-        *)
-            if [[ "$(grep "$1" $PROJ_FILE)" = "" ]]; then
-                echo "no project: $1"
-                return
-            fi
-            session="$1"
-            ppath="$(grep "$session" $PROJ_FILE)"
-            [[ -n $TMUX ]] && OP="switch" || OP="attach"
-            if ! $(tmux ls | grep -q "$session"); then
-                tmux new-session -s "$session" -d
-                tmux send-keys -t "$session:1" "cd $ppath" C-m
-                tmux send-keys -t "$session:1" "export PROJ=$ppath" C-m
-                tmux send-keys -t "$session:1" "clear" C-m
-            fi
-            tmux $OP -t "$session"
-            ;;
+    "a" | "add")
+        echo "$(pwd)" >>$PROJ_FILE
+        ;;
+    "e" | "edit")
+        $EDITOR $PROJ_FILE
+        ;;
+    "r" | "remove" | "rm")
+        if [ -z $2 ]; then
+            sed -i "" "/$(basename $(pwd))$/d" $PROJ_FILE
+        else
+            sed -i "" "/$2$/d" $PROJ_FILE
+        fi
+        ;;
+    *)
+        if [[ "$(grep "$1" $PROJ_FILE)" = "" ]]; then
+            echo "no project: $1"
+            return
+        fi
+        session="$1"
+        ppath="$(grep "$session" $PROJ_FILE)"
+        [[ -n $VTMUX ]] && OP="switch" || OP="attach"
+        if ! $(tmux ls | grep -q "$session"); then
+            tmux new-session -s "$session" -d
+            tmux send-keys -t "$session:1" "cd $ppath" C-m
+            tmux send-keys -t "$session:1" "export PROJ=$ppath" C-m
+            tmux send-keys -t "$session:1" "clear" C-m
+        fi
+        tmux $OP -t "$session"
+        ;;
     esac
 }
 # alias pj="project_switch"
 
 function vv() {
     case "$1" in
-        "")
-            name=$(basename $(pwd))
-            arg="."
-            ;;
-        ".")
-            name=$(basename $(pwd))
-            arg="."
-            ;;
-        *)
-            name=$(basename $1)
-            arg="$1"
-            ;;
+    "")
+        name=$(basename $(pwd))
+        arg="."
+        ;;
+    ".")
+        name=$(basename $(pwd))
+        arg="."
+        ;;
+    *)
+        name=$(basename $1)
+        arg="$1"
+        ;;
     esac
     tmux new-session -s $name -d
     tmux new-window -t $name -d
     tmux send-keys -t $name:1 "nvim $arg" Enter
-    if [ $TMUX ]; then
+    if [ $VTMUX ]; then
         tmux switch-client -t $name
     else
         tmux attach -t $name
@@ -126,24 +146,24 @@ function vv() {
 
 ts() {
     case "$1" in
-        "")
-            tmux list-sessions
-            ;;
-        *)
-            [[ -n $TMUX ]] && OP="switch" || OP="attach"
-            $(tmux ls | grep -q "$1") || tmux new-session -d -s "$1"
-            tmux -2 $OP -t "$1"
-            ;;
+    "")
+        tmux list-sessions
+        ;;
+    *)
+        [[ -n $VTMUX ]] && OP="switch" || OP="attach"
+        $(tmux ls | grep -q "$1") || tmux new-session -d -s "$1"
+        tmux -2 $OP -t "$1"
+        ;;
     esac
 }
 
 tn() {
     case "$1" in
-        "")
-            tmux rename-window "zsh"
-            ;;
-        *)
-            tmux rename-window "$1"
-            ;;
+    "")
+        tmux rename-window "zsh"
+        ;;
+    *)
+        tmux rename-window "$1"
+        ;;
     esac
 }
