4f60975070bf617f79cb94cbe73bf8a16f560de8
Lucas Crownover <lcrownover127@gmail.com>
2023-09-30 13:31:04 -0700

    updates again

--- nvim/lua/lcrownover/plugins/lspconfig.lua
+++ nvim/lua/lcrownover/plugins/lspconfig.lua
@@ -271,7 +271,6 @@ return {
 				Lua = {
 					runtime = { version = "LuaJIT" },
 					diagnostics = { globals = { "vim", "require" } },
-					-- workspace = { library = vim.api.nvim_get_runtime_file("", true) },
 					workspace = { checkThirdParty = false },
 					telemetry = { enable = false },
 				},
--- nvim/lua/lcrownover/plugins/treesitter.lua
+++ nvim/lua/lcrownover/plugins/treesitter.lua
@@ -1,31 +1,13 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	dependencies = { "nvim-treesitter/playground" },
-	event = "BufEnter",
-	build = ":TSUpdate",
-	config = function()
-		require("nvim-treesitter.configs").setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-			playground = {
-				enable = true,
-				disable = {},
-				updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-				persist_queries = false, -- Whether the query persists across vim sessions
-				keybindings = {
-					toggle_query_editor = "o",
-					toggle_hl_groups = "i",
-					toggle_injected_languages = "t",
-					toggle_anonymous_nodes = "a",
-					toggle_language_display = "I",
-					focus_language = "f",
-					unfocus_language = "F",
-					update = "R",
-					goto_node = "<cr>",
-					show_help = "?",
-				},
-			},
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = { "nvim-treesitter/playground" },
+    event = "BufEnter",
+    build = ":TSUpdate",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            auto_install = true,
+            highlight = { enable = true },
+            indent = { enable = true },
+        })
+    end,
 }
