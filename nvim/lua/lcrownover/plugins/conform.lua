e1661e0e6b4eed66043ba84cc4b3eca477c61f9e
Lucas Crownover <lcrownover127@gmail.com>
2023-10-02 11:35:10 -0700

    zsh formatting

--- nvim/lua/lcrownover/plugins/conform.lua
+++ nvim/lua/lcrownover/plugins/conform.lua
@@ -18,6 +18,7 @@ return {
         yaml = { "prettier" },
         markdown = { "prettier" },
         graphql = { "prettier" },
+        sh = { "shfmt" },
       },
       format_on_save = {
         lsp_fallback = true,
