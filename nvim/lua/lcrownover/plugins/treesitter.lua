3012517a6c1e3ea6e57b9239cf99e03938f7b28f
Lucas Crownover <lcrownover127@gmail.com>
2023-11-04 11:17:37 -0700

    update

--- nvim/lua/lcrownover/plugins/treesitter.lua
+++ nvim/lua/lcrownover/plugins/treesitter.lua
@@ -4,6 +4,7 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.configs").setup({
+      ensure_installed = { "markdown", "markdown_inline" },
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
