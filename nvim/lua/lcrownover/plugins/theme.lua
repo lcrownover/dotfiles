b6dea58ca7038a84c9a95841c0d875593697ac88
Lucas Crownover <lcrownover127@gmail.com>
2024-02-04 17:20:15 -0800

    best theme imo

--- nvim/lua/lcrownover/plugins/theme.lua
+++ nvim/lua/lcrownover/plugins/theme.lua
@@ -1,27 +1,27 @@
+-- return {
+--     "sainnhe/everforest",
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         vim.g.everforest_background = "medium"
+--         vim.g.everforest_better_performance = 1
+--         vim.cmd([[colorscheme everforest]])
+--         vim.g.lualine_theme = "everforest"
+--     end,
+-- }
+
 return {
-    "sainnhe/everforest",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        vim.g.everforest_background = "medium"
-        vim.g.everforest_better_performance = 1
-        vim.cmd([[colorscheme everforest]])
-        vim.g.lualine_theme = "everforest"
-    end,
+  "catppuccin/nvim",
+  name = "catppuccin",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      -- flavour = "frappe",
+      flavour = "macchiato",
+    })
+    vim.cmd.colorscheme("catppuccin")
+    vim.g.lualine_theme = "catppuccin"
+  end,
 }
 
--- return {
---   "catppuccin/nvim",
---   name = "catppuccin",
---   lazy = false,
---   priority = 1000,
---   config = function()
---     require("catppuccin").setup({
---       -- flavour = "frappe",
---       flavour = "macchiato",
---     })
---     vim.cmd.colorscheme("catppuccin")
---     vim.g.lualine_theme = "catppuccin"
---   end,
--- }
---
