bfa48ae2ae3670eec08905159c240fca24cde2b4
Lucas Crownover <lcrownover127@gmail.com>
2024-02-20 15:08:00 -0800

    colors

--- nvim/lua/lcrownover/plugins/theme.lua
+++ nvim/lua/lcrownover/plugins/theme.lua
@@ -26,6 +26,19 @@ return {
 }
 
 -- return {
+--   "sainnhe/gruvbox-material",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     vim.g.gruvbox_material_background = "hard"
+--     vim.g.gruvbox_material_enable_italic = 1
+--     vim.g.gruvbox_material_better_performance = 1
+--     vim.cmd.colorscheme("gruvbox-material")
+--     vim.g.lualine_theme = "gruvbox-material"
+--   end,
+-- }
+
+-- return {
 --   "Mofiqul/vscode.nvim",
 --   lazy = false,
 --   priority = 1000,
