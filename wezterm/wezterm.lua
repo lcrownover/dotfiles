363ed1f175b584e0e5ba127dee61e9384905e02c
Lucas Crownover <lcrownover127@gmail.com>
2024-03-03 22:53:11 -0800

    testing wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,29 @@
+-- Pull in the wezterm API
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+-- Start maximized
+local mux = wezterm.mux
+wezterm.on("gui-startup", function(cmd)
+  local _, _, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+-- Don't show the tab bar
+config.hide_tab_bar_if_only_one_tab = true
+
+-- Don't prompt to exit
+config.window_close_confirmation = "NeverPrompt"
+
+-- Padding around the window
+config.window_padding = { left = 8, right = 8, top = 8, bottom = 8 }
+
+-- Color settings
+config.color_scheme = "Gruvbox Material (Gogh)"
+
+-- Font settings
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 16.0
+
+-- and finally, return the configuration to wezterm
+return config
