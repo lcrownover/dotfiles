c252b423b3d75c09ec30b34b09abe914160e6012
Lucas Crownover <lcrownover127@gmail.com>
2024-03-06 14:13:39 -0800

    update none for new settings

--- nvim/lua/lcrownover/plugins/none-ls.lua
+++ nvim/lua/lcrownover/plugins/none-ls.lua
@@ -7,17 +7,12 @@ return {
       sources = {
         -- lua
         null_ls.builtins.formatting.stylua,
-        -- javascript
-        null_ls.builtins.diagnostics.eslint_d,
         -- python
         null_ls.builtins.formatting.isort,
-        null_ls.builtins.formatting.ruff,
-        null_ls.builtins.diagnostics.ruff,
         -- ansible
         null_ls.builtins.diagnostics.ansiblelint,
         -- sh
         null_ls.builtins.formatting.shfmt,
-        null_ls.builtins.diagnostics.shellcheck,
         -- markdown
         null_ls.builtins.diagnostics.markdownlint,
         -- everything else
