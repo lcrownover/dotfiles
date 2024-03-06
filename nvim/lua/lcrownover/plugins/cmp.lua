ec1f64754dfb6f383410bf595d9c36d6c6099c58
Lucas Crownover <lcrownover127@gmail.com>
2024-03-06 14:28:31 -0800

    update to cmp

--- nvim/lua/lcrownover/plugins/cmp.lua
+++ nvim/lua/lcrownover/plugins/cmp.lua
@@ -6,18 +6,8 @@ return {
     "hrsh7th/cmp-path",
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-nvim-lsp-signature-help",
-    "saadparwaiz1/cmp_luasnip",
     "onsails/lspkind-nvim",
     "neovim/nvim-lspconfig",
-    {
-      "L3MON4D3/LuaSnip",
-      version = "v2.*",
-      dependencies = { "rafamadriz/friendly-snippets" },
-      build = "make install_jsregexp",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
-    },
     "windwp/nvim-autopairs",
     "zbirenbaum/copilot-cmp",
   },
@@ -26,7 +16,7 @@ return {
     local lspkind = require("lspkind")
 
     cmp.setup({
-      mapping = {
+      mapping = cmp.mapping.preset.insert({
         ["<C-k>"] = cmp.mapping.select_prev_item(),
         ["<C-j>"] = cmp.mapping.select_next_item(),
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -34,25 +24,19 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      },
-      window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-      },
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
+      }),
+      -- window = {
+      --   completion = cmp.config.window.bordered(),
+      --   documentation = cmp.config.window.bordered(),
+      -- },
       -- sources for autocompletion
-      sources = {
+      sources = cmp.config.sources({
         { name = "nvim_lsp_signature_help" },
         { name = "nvim_lsp" },
         { name = "path" },
-        { name = "luasnip" },
         -- { name = "copilot" },
         { name = "buffer" },
-      },
+      }),
       -- configure completion icons
       formatting = {
         format = lspkind.cmp_format({
