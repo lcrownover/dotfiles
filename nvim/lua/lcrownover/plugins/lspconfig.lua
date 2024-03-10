10fdc3ef7ad1e40fa7bb9773a3194877eb0d15b0
Lucas Crownover <lcrownover127@gmail.com>
2024-03-09 19:27:01 -0800

    fix no snippets bug

--- nvim/lua/lcrownover/plugins/lspconfig.lua
+++ nvim/lua/lcrownover/plugins/lspconfig.lua
@@ -160,6 +160,7 @@ return {
 
     local lsp = require("lspconfig")
     local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+    lsp_capabilities.textDocument.completion.completionItem.snippetSupport = false
 
     ---------------------------------------
     -- rust
