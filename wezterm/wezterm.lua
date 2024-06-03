5698b5c2ebb43a4144809f6c343c3b0bff1018ef
Lucas Crownover <lcrownover127@gmail.com>
2024-06-03 09:42:32 +0100

    wezterm with panes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -16,18 +16,39 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = "NeverPrompt"
 
 -- Padding around the window
-config.window_padding = { left = 8, right = 8, top = 8, bottom = 8 }
+config.window_padding = { left = 8, right = 8, top = 8, bottom = 0 }
 
 -- Color settings
 -- config.color_scheme = "Everforest Dark (Gogh)"
--- config.color_scheme = "Gruvbox Material (Gogh)"
+config.color_scheme = "Catppuccin Macchiato"
 
 -- Font settings
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = 'Medium' })
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
 config.font_size = 16.0
 config.line_height = 1.05
 
 config.freetype_render_target = "HorizontalLcd"
+
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+
+config.keys = {
+    { key = "m",  mods = "ALT", action = wezterm.action.TogglePaneZoomState },
+    { key = "c",  mods = "ALT", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+    { key = "n",  mods = "ALT", action = wezterm.action.ActivateTabRelative(1) },
+    { key = "p",  mods = "ALT", action = wezterm.action.ActivateTabRelative(-1) },
+    { key = "x",  mods = "ALT", action = wezterm.action.CloseCurrentTab({ confirm = true }) },
+    { key = "w",  mods = "ALT", action = wezterm.action.CloseCurrentPane({ confirm = true }) },
+    { key = "-",  mods = "ALT", action = wezterm.action.SplitVertical },
+    { key = "\\", mods = "ALT", action = wezterm.action.SplitHorizontal },
+    { key = "h",  mods = "ALT", action = wezterm.action.ActivatePaneDirection("Left") },
+    { key = "l",  mods = "ALT", action = wezterm.action.ActivatePaneDirection("Right") },
+    { key = "k",  mods = "ALT", action = wezterm.action.ActivatePaneDirection("Up") },
+    { key = "j",  mods = "ALT", action = wezterm.action.ActivatePaneDirection("Down") },
+    { key = "r",  mods = "ALT", action = wezterm.action.RotatePanes("CounterClockwise") },
+}
 
 -- and finally, return the configuration to wezterm
 return config
