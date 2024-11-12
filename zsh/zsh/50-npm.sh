c506b437e768bb5bb509f2f6e98decc7eb478733
Lucas Crownover <lcrownover127@gmail.com>
2024-11-11 18:26:20 -0800

    nvm

--- zsh/zsh/50-npm.sh
+++ zsh/zsh/50-npm.sh
@@ -1,2 +1,6 @@
 append_path "$HOME/.npm/bin"
 export NODE_PATH=$NODE_PATH:$HOME/.npm/lib/node_modules
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
